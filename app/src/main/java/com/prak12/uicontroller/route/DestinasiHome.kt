7f4e4610fc7fbb0d1dd0951b95e8b15f82867d5d
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-12-18 18:55:34 +0700

    buat file DestinasiHome.kt di package uicontroller.route

---
+++ app/src/main/java/com/prak12/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.prak12.uicontroller.route
+
+import com.prak12.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
