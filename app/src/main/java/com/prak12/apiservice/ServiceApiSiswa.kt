ada94c900088ddea377547b7963c60a01ac87569
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-12-18 09:29:36 +0700

    tambahkan data class DataSiswa, UIStateSiswa, DetailSiswa, dan fungsi extension toDataSiswa ke DataSiswa.kt

--- app/src/main/java/com/prak12/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/prak12/apiservice/ServiceApiSiswa.kt
@@ -1,4 +1,22 @@
 package com.prak12.apiservice
 
+import com.prak12.modeldata.DataSiswa
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Body
+
 interface ServiceApiSiswa {
+
+    @GET("bacateman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body datasiswa: DataSiswa):retrofit2.Response<Void>
+
+//  @GET("baca1teman.php/{id}")
+//  suspen fun getStatusSiswa(@Query("id") id: int): DataSiswa
+
+//  @PUT("editTM.php/{id}")
+//  suspend fun editStatusSiswa(@Query("id") id: int, @Body datasiswa: DataSiswa): retrofit2.Response<Void>
+
 }
