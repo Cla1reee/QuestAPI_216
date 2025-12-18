78ddacea22149e50d20a1046bdc6bb19278aef96
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-12-18 18:56:56 +0700

    buat interface RepositoryDataSiswa dan class JaringanRepositoryDataSiswa di package repositori

--- app/src/main/java/com/prak12/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/prak12/repositori/RepositoryDataSiswa.kt
@@ -1,2 +1,17 @@
 package com.prak12.repositori
 
+import com.prak12.apiservice.ServiceApiSiswa
+import com.prak12.modeldata.DataSiswa
+import retrofit2.Response
+
+interface RepositoryDataSiswa{
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDatasiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDatasiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
