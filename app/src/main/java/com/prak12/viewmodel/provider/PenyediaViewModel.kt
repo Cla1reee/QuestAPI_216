e983626f0a8591f71d41bf14dc97998c08cc9e0a
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-12-18 18:58:17 +0700

    buat file PenyediaViewModel.kt di package viewmodel.provider

---
+++ app/src/main/java/com/prak12/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,23 @@
+package com.prak12.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.prak12.repositori.AplikasiDataSiswa
+import com.prak12.viewmodel.EntryViewModel
+import com.prak12.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+    }
+}
