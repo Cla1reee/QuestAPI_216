aca4b41d9b70fa6ee08487c1e4f5e690e6d7761c
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-12-18 09:33:38 +0700

    implementasikan interface ContainerApp dan class DefaultContainerApp di ContainerApp.kt

--- app/src/main/java/com/prak12/repositori/ContainerApp.kt
+++ app/src/main/java/com/prak12/repositori/ContainerApp.kt
@@ -1,2 +1,24 @@
 package com.prak12.repositori
 
+import com.prak12.apiservice.ServiceApiSiswa
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+
+interface ContainerApp {
+    val repositoryDataSiswa: RepositoryDataSiswa
+}
+
+class DefaultContainerApp : ContainerApp{
+    private val baseurl = "http://10.0.2.2/umyTI"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val klien = OkHttpClient.Builder()
+
+}
