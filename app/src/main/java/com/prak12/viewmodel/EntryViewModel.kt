e31df72d5b507e41717fa2f8b0a6a5054707616e
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-12-18 18:56:34 +0700

    buat file EntryViewModel.kt di package viewmodel

---
+++ app/src/main/java/com/prak12/viewmodel/EntryViewModel.kt
@@ -0,0 +1,45 @@
+package com.prak12.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.prak12.modeldata.DetailSiswa
+import com.prak12.modeldata.UIStateSiswa
+import com.prak12.modeldata.toDataSiswa
+import com.prak12.repositori.RepositoryDataSiswa
+import retrofit2.Response
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ):
+            Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    //Fungsi untuk menangani saat ada perubahan pada text input
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
+                (detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            val sip: Response<Void> = repositoryDataSiswa.postDatasiswa(uiStateSiswa
+                .detailSiswa.toDataSiswa())
+            if (sip.isSuccessful) {
+                println("Sukses Tambah Data : ${sip.message()}")
+            } else {
+                println("Gagal tambah data : ${sip.errorBody()}")
+            }
+        }
+    }
+}
